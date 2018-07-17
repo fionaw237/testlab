ec9816942890d382e3b4d23568b87911d09bd738
martin <martinshek8@gmail.com>
2018-07-17 15:11:15 +0100

    Ruby Functions Initial Commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,69 @@
+def return_10
+  return 10
+end
+
+def add(first_number, second_number)
+  return first_number + second_number
+end
+
+def subtract(first_number, second_number)
+  return first_number - second_number
+end
+
+def multiply(first_number, second_number)
+  return first_number * second_number
+end
+
+def divide(first_number, second_number)
+  return first_number / second_number
+end
+
+def length_of_string(my_string)
+  return my_string.length
+end
+
+def join_string( string_1, string_2 )
+  return string_1 + string_2
+end
+
+def add_string_as_number( string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name(month_number)
+  months = {
+  1 => "January",
+  2 => "February",
+  3 => "March",
+  4 => "April",
+  5 => "May",
+  6 => "June",
+  7 => "July",
+  8 => "August",
+  9 => "September",
+  10 => "October",
+  11 => "November",
+  12 => "December",
+  }
+  return months[month_number]
+end
+
+
+def number_to_short_month_name(month_number)
+  months = {
+  1 => "Jan",
+  2 => "Feb",
+  3 => "Mar",
+  4 => "Apr",
+  5 => "May",
+  6 => "Jun",
+  7 => "Jul",
+  8 => "Aug",
+  9 => "Sep",
+  10 => "Oct",
+  11 => "Nov",
+  12 => "Dec",
+  }
+  return months[month_number]
+end
+   
